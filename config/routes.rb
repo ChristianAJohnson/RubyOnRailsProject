621edc03403369567739ec5fcad5a8d728744ab0
ChristianAJohnson <chrisgochris@gmail.com>
2018-05-31 19:56:32 -0400

    first ruby on rails project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'students/index'
+  # get 'students/new'
+  # get 'students/edit'
+  # get 'students/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students
+end
